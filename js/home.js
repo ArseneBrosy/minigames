1b29b789aa15dc9a86ae7c00ad84a5632154ed4b
ArseneBrosy <arsene.brosy@gmail.com>
2025-06-11 11:18:06 +0200

    enter code

--- js/home.js
+++ js/home.js
@@ -7,4 +7,35 @@ function generateCode(length = 5) {
   return code;
 }
 
+function codeInputAction(input) {
+  event.preventDefault();
+
+  const key = event.key.toUpperCase();
+  if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".includes(key)) {
+    document.querySelectorAll("#code input")[input].value = key;
+    document.querySelectorAll("#code input")[input].classList.add("filled");
+    if (input < 4) {
+      document.querySelectorAll("#code input")[input + 1].select();
+    } else {
+      const value =
+          document.querySelectorAll("#code input")[0].value +
+          document.querySelectorAll("#code input")[1].value +
+          document.querySelectorAll("#code input")[2].value +
+          document.querySelectorAll("#code input")[3].value +
+          document.querySelectorAll("#code input")[4].value;
+      console.log(value);
+    }
+  }
+  if (key === "BACKSPACE") {
+    if (document.querySelectorAll("#code input")[input].value === "") {
+      document.querySelectorAll("#code input")[input - 1].value = "";
+      document.querySelectorAll("#code input")[input - 1].classList.remove("filled");
+      document.querySelectorAll("#code input")[input - 1].select();
+    } else {
+      document.querySelectorAll("#code input")[input].value = "";
+      document.querySelectorAll("#code input")[input].classList.remove("filled");
+    }
+  }
+}
+
 document.querySelector('#private-lobby-button').href = `./game.html?room=${generateCode()}`;
